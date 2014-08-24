a153d0358f708bb37d8dae63a0f28102b3fc4c8a
Scott Basgaard <sbasgaard@gmail.com>
2014-08-24 12:21:11 +0200

    Remove second line of footer credits

--- footer.php
+++ footer.php
@@ -13,8 +13,6 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
 			<a href="http://wordpress.org/" rel="generator"><?php printf( __( 'Proudly powered by %s', 'wordsesh-theme' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'wordsesh-theme' ), 'wordsesh-theme', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->

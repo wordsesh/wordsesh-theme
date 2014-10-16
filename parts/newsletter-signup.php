d4bc0a68a86518329dbb8c18e4784c1c40fbb3fe
Scott Basgaard <sbasgaard@gmail.com>
2014-10-16 17:27:34 +0100

    Started styling Gravity Forms sign-up

--- parts/newsletter-signup.php
+++ parts/newsletter-signup.php
@@ -1,5 +1,25 @@
-<form id="newsletter-signup-form" class="newsletter-signup-form" action="" method="GET">
-  
-	<input type="text" name="newsletter-signup" placeholder="<?php esc_attr_e( 'Enter your email to subscribe and stay updated.', 'wordsesh' ); ?>">
 
-</form>
+<?php while ( have_posts() ) : the_post(); ?>
+
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+		<header class="entry-header">
+		</header><!-- .entry-header -->
+
+		<div class="entry-content">
+			<?php the_content(); ?>
+			<?php
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . __( 'Pages:', 'wordsesh-theme' ),
+					'after'  => '</div>',
+				) );
+			?>
+		</div><!-- .entry-content -->
+
+		<footer class="entry-footer">
+			<?php edit_post_link( __( 'Edit', 'wordsesh-theme' ), '<span class="edit-link">', '</span>' ); ?>
+		</footer><!-- .entry-footer -->
+
+	</article><!-- #post-## -->
+
+<?php endwhile; // end of the loop. ?>
--- templates/attending.php
+++ templates/attending.php
@@ -58,33 +58,8 @@ Template Name: Attending
 
 		<div id="primary" class="content-area">
 			<main id="main" class="site-main" role="main">
-
-				<?php while ( have_posts() ) : the_post(); ?>
-
-					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					
-						<header class="entry-header">
-						</header><!-- .entry-header -->
-
-						<div class="entry-content">
-							<?php the_content(); ?>
-							<?php
-								wp_link_pages( array(
-									'before' => '<div class="page-links">' . __( 'Pages:', 'wordsesh-theme' ),
-									'after'  => '</div>',
-								) );
-							?>
-						</div><!-- .entry-content -->
-						
-						<?php get_template_part( 'parts/attendees' ); ?>
-
-						<footer class="entry-footer">
-							<?php edit_post_link( __( 'Edit', 'wordsesh-theme' ), '<span class="edit-link">', '</span>' ); ?>
-						</footer><!-- .entry-footer -->
-
-					</article><!-- #post-## -->
-
-				<?php endwhile; // end of the loop. ?>
+				
+				<?php get_template_part( 'parts/attendees' ); ?>
 
 			</main><!-- #main -->
 		</div><!-- #primary -->

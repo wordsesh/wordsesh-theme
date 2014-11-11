d09cbc57768ebb2c11539d43a8591f1e579f1622
Scott Basgaard <sbasgaard@gmail.com>
2014-11-11 11:52:58 +0100

    Display all attendees

--- includes/homepage/attendees.php
+++ includes/homepage/attendees.php
@@ -36,7 +36,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 	<?php
 		$limit = intval( $settings['homepage_our_team_number'] );
-		do_action( 'woothemes_our_team', array( 'limit' => '50' ) );
+		do_action( 'woothemes_our_team', array( 'limit' => '-1' ) );
 	?>
 
 </section><!-- /#our-team -->

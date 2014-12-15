4dee830d7657c0b444d8a96a26ee43a99899c3c8
Scott Basgaard <sbasgaard@gmail.com>
2014-12-15 16:45:00 +0100

    Add attendee count copy

--- includes/homepage/attendees.php
+++ includes/homepage/attendees.php
@@ -31,6 +31,10 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 			<?php if ( '' != $settings['homepage_our_team_title'] ): ?><h1><?php echo stripslashes( esc_html( $settings['homepage_our_team_title'] ) ); ?></h1><?php endif; ?>
 		</header>
 		<?php endif; ?>
+		
+		<?php $attendee_count = wp_count_posts( 'speaker' )->publish; ?>
+		
+		<h2 class="attendee-count">That's just a few. There are over <span><?php echo $attendee_count; ?></span> of them!<h2>
 
 	</div><!-- /.wrapper -->
 

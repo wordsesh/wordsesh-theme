2a78a4cbc507e99316024c6e98a306fb47a45c87
Scott Basgaard <sbasgaard@gmail.com>
2014-12-18 17:02:02 +0100

    Remove attendee signup-up form, get ready for coundtdown / live.

--- includes/homepage/intro-message.php
+++ includes/homepage/intro-message.php
@@ -25,18 +25,56 @@ $settings = woo_get_dynamic_values( $settings );
 <section id="intro-message" class="home-section">
 
 	<div class="wrapper">
+		
+		<?php if ( wordsesh_is_live() ) : ?>
 
-		<?php if ( '' != $settings['homepage_intro_message_heading'] ): ?>
-			<h2><?php echo esc_attr( $settings['homepage_intro_message_heading'] ); ?></h2>
+			<?php if ( '' != $settings['homepage_intro_message_heading'] ): ?>
+				<h2><?php echo esc_attr( $settings['homepage_intro_message_heading'] ); ?></h2>
+			<?php endif; ?>
+
+			<?php if ( '' != $settings['homepage_intro_message_content'] ): ?>
+				<?php echo wpautop( stripslashes( esc_attr( $settings['homepage_intro_message_content'] ) ) ); ?>
+			<?php endif; ?>		
+
+			<?php if ( ( '' != $settings['homepage_intro_message_button_label'] ) && ( '' != $settings['homepage_intro_message_button_url'] ) ): ?>
+				<a class="button" href="<?php echo esc_url( $settings['homepage_intro_message_button_url'] ); ?>"><?php echo esc_attr( $settings['homepage_intro_message_button_label'] ); ?></a>
+			<?php endif; ?>
+			
+		<?php else : ?>
+			
+			<div class="clock animated slideInRight">
+				<div class="canvas">
+					<canvas id="days" width="148" height="148"></canvas>
+					<div class="info">
+						<p class="days-val">0</p>
+						<p class="text">Days</p>
+					</div>
+				</div>
+				<div class="canvas">
+					<canvas id="hours" width="148" height="148"></canvas>
+					<div class="info">
+						<p class="hours-val">0</p>
+						<p class="text">Hours</p>
+					</div>
+				</div>
+				<div class="canvas">
+					<canvas id="minutes" width="148" height="148"></canvas>
+					<div class="info">
+						<p class="minutes-val">0</p>
+						<p class="text">Minutes</p>
+					</div>
+				</div>
+				<div class="canvas last-one">
+					<canvas id="seconds" width="148" height="148"></canvas>
+					<div class="info">
+						<p class="seconds-val">0</p>
+						<p class="text">Seconds</p>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		
 		<?php endif; ?>
-
-		<?php if ( '' != $settings['homepage_intro_message_content'] ): ?>
-			<?php echo wpautop( stripslashes( esc_attr( $settings['homepage_intro_message_content'] ) ) ); ?>
-		<?php endif; ?>		
-
-		<?php if ( ( '' != $settings['homepage_intro_message_button_label'] ) && ( '' != $settings['homepage_intro_message_button_url'] ) ): ?>
-			<a class="button" href="<?php echo esc_url( $settings['homepage_intro_message_button_url'] ); ?>"><?php echo esc_attr( $settings['homepage_intro_message_button_label'] ); ?></a>
-		<?php endif; ?> 
 
 	</div><!-- /.wrapper -->
 
---
+++ js/wordsesh.js
@@ -0,0 +1,10 @@
+
+jQuery( document ).ready( function( $ ) {
+
+    $( ".clock" ).countDownTimer({
+    	color 	: '#2d8c73',
+    	endDate : new Date(Date.UTC(2014, 11, 20, 0, 0, 0))
+    });
+	
+	
+});

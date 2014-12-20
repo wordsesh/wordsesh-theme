449fc50522421c0740dffb7b146a657be0c6f546
Scott Basgaard <sbasgaard@gmail.com>
2014-12-20 01:18:38 +0100

    Fix crowdcast embed height again

--- page-live.php
+++ page-live.php
@@ -11,7 +11,7 @@ get_header();
 	 
 	 <div class="wrapper">
 
-		 <iframe width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" allowtransparency="true" src="http://crowdcast.io/e/wordsesh?navlinks=false"></iframe>
+		 <iframe width="100%" height="720" frameborder="0" marginheight="0" marginwidth="0" allowtransparency="true" src="http://crowdcast.io/e/wordsesh?navlinks=false"></iframe>
 		 
 	 </div>
 
